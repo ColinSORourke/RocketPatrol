96b6bde012258684f0c9b5f7e567acd2c64c4bd8
ColinSORourke <csorourk@ucsc.edu>
2021-04-12 16:55:39 -0700

    Github is bad and case sensitive on dir names

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -5,12 +5,12 @@ class Play extends Phaser.Scene {
 
     preload() {
         // load images/tile sprites
-        this.load.image('rocket', './assets/rocket.png');
-        this.load.image('spaceship', './assets/spaceship.png');
-        this.load.image('starfield', './assets/starfield.png');
+        this.load.image('rocket', './Assets/rocket.png');
+        this.load.image('spaceship', './Assets/spaceship.png');
+        this.load.image('starfield', './Assets/starfield.png');
 
         // those juicy animations
-        this.load.spritesheet('explosion', './assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
+        this.load.spritesheet('explosion', './Assets/explosion.png', {frameWidth: 64, frameHeight: 32, startFrame: 0, endFrame: 9});
       }
 
     create() {
